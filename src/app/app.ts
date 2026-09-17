b0eb0f64be44d8a5a7230c155a0a9b42adb0ca5c
DC <gitcodegallery@gmail.com>
2026-09-17 00:35:35 -0400

    Created starter Angular file and completed Assignment 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('Darius-Cherry-Learning-AngularF26');
+  fName = "Darius";
+  lName = "Cherry"
 }
