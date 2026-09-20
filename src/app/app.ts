8a1021866f33ce65353846ca41181a6ff7038b93
DC <gitcodegallery@gmail.com>
2026-09-20 00:29:14 -0400

    - initialized array with 6 instances -Used a @for to loop over my arrays - Added an @empty so page shows something is anything is empty - Used @if to show and hide the friendsAge -Added some SCSS - Included a @media query

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Assignment2 } from './shared/models/assignment2'; // imports the information from the Assignment2 folder
 
 @Component({
   imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.css',
-  templateUrl: './app.html',
+  styleUrl: './app.scss', //Links to the app.scss folder containing the styles that I have set
+  templateUrl: './app.html', // Connects to the app.html folder that I have set
 })
 export class App {
-  fName = "Darius";
-  lName = "Cherry"
+  title = 'Assignment 2 Instructions';
+
+  assignment2List: Assignment2[] = [ // Fills in the information that was set on the Assignment2.ts folder
+    { id: 1, name: 'Mark', city: 'Windsor', age: 18, friendAge: true, howManyHouse: "one" },
+    { id: 2, name: 'James', city: 'Toronto', age: 19, friendAge: false,},
+    { id: 3, name: 'Charles', city: 'Ottawa', age: 20, friendAge: true, howManyHouse: 1},
+  ];
 }
--- src/app/shared/models/assignment2.ts
+++ src/app/shared/models/assignment2.ts
@@ -1,12 +1,11 @@
 
-export interface Assignment2 {
+export interface Assignment2 { // export makes it where this can be used in multiple different places as long as you import it
   id: number;
   name: string;
   city: string;
   age: number;
-  hasBigHouse?: boolean; //This is an optional property
-  friendAge: number | string;
-
+  howManyHouse?: number | string; //This is an optional property
+  friendAge: boolean;// This is a union type meaning it accepts a number(int,doubles,floats) or strings ("Something")
 }
 
 
