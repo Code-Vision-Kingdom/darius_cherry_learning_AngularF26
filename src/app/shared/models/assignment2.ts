54d1ec21e67ada3879140a8ac88f0793fed44315
DC <gitcodegallery@gmail.com>
2026-09-21 14:54:10 -0400

    Deleted the SCSS To add different Styles and changed the option to the last position instead of in the middle

--- src/app/shared/models/assignment2.ts
+++ src/app/shared/models/assignment2.ts
@@ -4,8 +4,8 @@ export interface Assignment2 { // export makes it where this can be used in mult
   name: string;
   city: string;
   age: number;
-  howManyHouse?: number | string; //This is an optional property
   friendAge: boolean;// This is a union type meaning it accepts a number(int,doubles,floats) or strings ("Something")
+  howManyHouse?: number | string; //This is an optional property
 }
 
 
