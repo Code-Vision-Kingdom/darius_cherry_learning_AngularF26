2b5e607a6594fa5541930fefacf0c6aa9280f614
DC <gitcodegallery@gmail.com>
2026-09-24 01:01:57 -0400

    - Changed the names in my assignment2.ts to valid names - added a hover in my scss for more design - Changed the information in app.ts to match with my assignment2.ts got rid of the beginning of the HTML file that was not needed for the app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Assignment2 } from './shared/models/assignment2'; // imports the information from the Assignment2 folder
+import { Weapon } from './shared/models/assignment2'; // imports the information from the Assignment2 folder
 
 @Component({
   imports: [RouterOutlet],
@@ -11,13 +11,54 @@ import { Assignment2 } from './shared/models/assignment2'; // imports the inform
 export class App {
   title = 'Assignment 2 Instructions';
 
-  assignment2List: Assignment2[] = [
+  weaponList: Weapon[] = [
     // Fills in the information that was set on the Assignment2.ts folder
-    { id: 1, name: 'Mark', city: 'Windsor', age: 18, friendAge: 1, howManyHouse: false},
-    { id: 2, name: 'James', city: 'Toronto', age: 19, friendAge: 'one', howManyHouse: false },
-    { id: 3, name: 'Charles', city: 'Ottawa', age: 20, friendAge: 2, howManyHouse: "Yes" },
-    { id: 4, name: 'Michael', city: 'Waterloo', age: 21, friendAge: 'two', howManyHouse: "Yes" },
-    { id: 5, name: 'Jackson', city: 'Brampton', age: 22, friendAge: 3, howManyHouse: "Yes" },
-    { id: 6, name: 'Kaleb', city: 'Chatham', age: 23, friendAge: 'three', howManyHouse: false },
+    { id: 1,
+      name: 'Ice Blade',
+      damage: 100,
+      rarity: 'Rare',
+      class: 'Melee',
+      realItem: true
+    },
+    {
+      id: 2,
+      name: 'Muramasa',
+      damage: 150,
+      rarity: 'Epic',
+      class: 'Melee',
+      realItem: false
+    },
+    {
+      id: 3,
+      name: 'Nights Edge',
+      damage: 200,
+      rarity: 'Legendary',
+      class: 'Melee',
+      realItem: true,
+    },
+    {
+      id: 4,
+      name: 'Eventide',
+      damage: 250,
+      rarity: 'Rare',
+      class: 'Ranged',
+      realItem: false
+    },
+    {
+      id: 5,
+      name: 'Nights Edge',
+      damage: 300,
+      rarity: 'Epic',
+      class: 'Ranged',
+      realItem: true },
+    {
+      id: 6,
+      name: 'Nights Edge',
+      damage: 200,
+      rarity: 'Legendary',
+      class: 'Ranged',
+      realItem: false
+    },
+
   ];
 }
--- src/app/shared/models/assignment2.ts
+++ src/app/shared/models/assignment2.ts
@@ -1,11 +1,11 @@
 
-export interface Assignment2 { // export makes it where this can be used in multiple different places as long as you import it
+export interface Weapon { // export makes it where this can be used in multiple different places as long as you import it
   id: number;
   name: string;
-  city: string;
-  age: number;
-  friendAge: number | string;// This is a union type meaning it accepts a number(int,doubles,floats) or strings ("Something")
-  howManyHouse?: boolean | string; //This is an optional property
+  damage: number;
+  rarity: string;
+  class: "Melee" | "Ranged";// This is a union type meaning it accepts a number(int,doubles,floats) or strings ("Something")
+  realItem?: boolean; //This is an optional property
 }
 
 
